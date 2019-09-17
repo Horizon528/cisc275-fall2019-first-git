e8a8c214e00eea2fdc9f77bb09326b77a7c92b82
AntLeung <36493128+Horizon528@users.noreply.github.com>
2019-09-16 21:16:31 -0400

    Create dog.java

---
+++ dog.java
@@ -0,0 +1,10 @@
+public class Dog extends Animal
+{
+    public Dog(String n, int l){
+        super(n, l);
+    }
+    public String toString(){
+        String le = String.valueOf(legs);
+        return (name + " has " + le + " legs");
+    }
+}
